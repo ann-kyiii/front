9ae1654f196217fbb61717dc1492f7d9e3852ecf
Dai7Igarashi <40178733+Dai7Igarashi@users.noreply.github.com>
2019-09-21 22:42:58 +0900

    ci: yarn fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
 const App: React.FC = () => (
-  <React.Fragment>
+  <>
     <header />
     <footer />
-  </React.Fragment>
+  </>
 );
 
 export default App;
