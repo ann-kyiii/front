ed339e038ba8e7fbdf3bf93ca5680020f8195bab
iwagithub <iwakami-tomoya-ch@ynu.jp>
2019-11-17 12:51:57 +0900

    add: borrow api

---
+++ src/apis/fetchBorrow.ts
@@ -0,0 +1,18 @@
+type FetchBorrowPayload = {
+    id: number;
+    name: string;
+  };
+  
+  export const fetchBorrow = (payload: FetchBorrowPayload) => {
+    return fetch(`${process.env.REACT_APP_API_HOST}/borrow/`, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+      });
+  };
+  
+  export default fetchBorrow;
+  
