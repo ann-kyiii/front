13f8c4487310deca6d640a69c430d09273a683e9
iwagithub <iwakami-tomoya-ch@ynu.jp>
2019-11-17 13:01:03 +0900

    add: id to bookTitle

--- src/components/organisms/BorrowButton/BorrowButton.tsx
+++ src/components/organisms/BorrowButton/BorrowButton.tsx
@@ -3,67 +3,113 @@ import cx from "classnames";
 import { useDispatch, useSelector } from "react-redux";
 import { push } from "connected-react-router";
 import { useModal } from "react-modal-hook";
+import { get } from "lodash";
+import { normalize, schema } from "normalizr";
 import { AppProps } from "../../../App";
 import styles from "./BorrowButton.module.css";
+import fetchBookLists, {
+  BookLists,
+  BooksState
+} from "../../../actions/resultlists";
+import fetchBorrow from "../../../apis/fetchBorrow";
 
 type BorrowButtonProps = AppProps & {
   className?: string[];
   buttonName: string;
   bookTitle: string;
   borrower: string;
+  bookId: number;
 };
 
 export const BorrowButton = (props: BorrowButtonProps) => {
-  const { className, buttonName, bookTitle, borrower } = props;
+  const { className, buttonName, bookTitle, borrower, bookId } = props;
   const dispatch = useDispatch();
 
-  const testJson = { books: [
-    {
-    ISBN:"9784061529014",
-    author:"杉山将／著",
-    bookName:"機械学習のための確率と統計",
-    borrower: [],
-    exist:"一部発見",
-    find:3,
-    genre:"研究(理論)",
-    id:278,
-    location:"unidentified",
-    other:"なし",
-    pubdate:"20150408",
-    publisher:"講談社サイエンティフィク",
-    subGenre:"統計・機械学習",
-    sum:6,
-    withDisc:"なし",
-    },
-    {
-    ISBN:"9784061529021",
-    author:"岡谷貴之／著",
-    bookName:"深層学習",
-    borrower: [],
-    exist:"一部発見",
-    find:1,
-    genre:"研究(理論)",
-    id:279,
-    location:"unidentified",
-    other:"なし",
-    pubdate:"20150408",
-    publisher:"講談社サイエンティフィク",
-    subGenre:"ニューラルネットワーク",
-    sum:2,
-    withDisc:"なし",
-    },
-    ] }
+  // const testJson = { books: [
+  //   {
+  //   ISBN:"9784061529014",
+  //   author:"杉山将／著",
+  //   bookName:"機械学習のための確率と統計",
+  //   borrower: [],
+  //   exist:"一部発見",
+  //   find:3,
+  //   genre:"研究(理論)",
+  //   id:278,
+  //   location:"unidentified",
+  //   other:"なし",
+  //   pubdate:"20150408",
+  //   publisher:"講談社サイエンティフィク",
+  //   subGenre:"統計・機械学習",
+  //   sum:6,
+  //   withDisc:"なし",
+  //   },
+  //   {
+  //   ISBN:"9784061529021",
+  //   author:"岡谷貴之／著",
+  //   bookName:"深層学習",
+  //   borrower: [],
+  //   exist:"一部発見",
+  //   find:1,
+  //   genre:"研究(理論)",
+  //   id:279,
+  //   location:"unidentified",
+  //   other:"なし",
+  //   pubdate:"20150408",
+  //   publisher:"講談社サイエンティフィク",
+  //   subGenre:"ニューラルネットワーク",
+  //   sum:2,
+  //   withDisc:"なし",
+  //   },
+  //   ] }
 
-    
+  const normalizeData = (
+    data: BookLists
+  ): Pick<BooksState, "booksTable" | "booksIdList"> => {
+    const booksSchema = new schema.Entity("books", {}, { idAttribute: "id" });
+    const booksTable = get(normalize(data, [booksSchema]), [
+      "entities",
+      "books"
+    ]);
+    const booksIdList = get(normalize(data, [booksSchema]), ["result"]);
+    return { booksTable, booksIdList };
+  };
+
+  // サーバにidと名前を送り，redux更新
+  const sendBorrowerName = async () => {  
+    const payload = {
+      id: bookId,
+      name: borrower,
+    };
+    try {
+      dispatch(fetchBookLists.started({ pageIndex: 0 }));
+      const response = await fetchBorrow(payload);
+      // const response = await fetch("dummyData.json");
+      // if (!response.ok) {
+      //   dispatch(
+      //     fetchBookLists.failed({
+      //       params: { pageIndex: page },
+      //       error: { statusCode: response.status }
+      //     })
+      //   );
+      //   return;
+      // }
+      const json = await response.json();
+      // APIのreturnが books: {} なので.
+      const newData = normalizeData(json.books);
+      const result = { ...newData, maxBooks: json.max_books };
+      dispatch(fetchBookLists.done({ params: { pageIndex: 0 }, result }));
+    } catch (error) {
+      console.log(`Error fetcing in getBookLists: ${error}`);
+    }
+  };
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    const encode = encodeURI(`/book-lists`);
+    sendBorrowerName();
+    const encode = encodeURI(`/book-lists/` + bookId);
     dispatch(push(encode));
     props.history.push(encode);
   }
 
-  // console.log(borrower);
-  //TODO:onClickではサーバに送る
   const [showModal, hideModal] = useModal(() => (
     <div className={styles.wrapper}>
       <div role="dialog" className={styles.modal}>
@@ -87,7 +133,7 @@ export const BorrowButton = (props: BorrowButtonProps) => {
       type="button"
       aria-label="Submit"
       //   onKeyDown={e => (e.key === "Enter" ? handleClick : null)}
-      onClick={showModal}
+      onClick={borrower != "" ? showModal: undefined }
       className={styles.BorrowButton}
       //   className={cx(
       //     styles.IconRect,
--- src/components/templates/Borrow.tsx
+++ src/components/templates/Borrow.tsx
@@ -1,106 +1,121 @@
-import React, {useState, useCallback} from "react";
+import React, {useState, useCallback, useEffect} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppProps } from "../../App";
+import { push } from "connected-react-router";
 import Header from "../organisms/Header";
-import SearchBox from "../atoms/SearchBox";
 import BookName from "../molecules/BookName"
-// import PopupWindow from "../molecules/PopupWindow"
-import SearchIcon from "../atoms/SearchIcon"
 import BorrowButton from "../organisms/BorrowButton"
 import BorrowUserNameInput from "../molecules/BorrowUserNameInput"
 import { RootState } from "../../reducers";
 import { isEmpty, get } from "lodash";
+import { normalize, schema } from "normalizr";
 import fetchBookLists, {
   BookLists,
-  SavedBooks
+  BooksState
 } from "../../actions/resultlists";
-import { normalize, schema } from "normalizr";
-
+import fetchBookId from "../../apis/fetchBookId";
 import { ModalProvider, useModal } from "react-modal-hook";
-// import Dialog from "../molecules/Dialog"
 
 export const Borrow = ({ history }: AppProps) =>  {
   const [borrower, setBorrower] = useState<string>("");
+    // URLからid取得
+  const search: string = useSelector((state: RootState) =>
+    get(state, ["router", "location", "pathname"])
+  ).slice(1);
+  const decode: string = decodeURI(search);
+  const urlParams: string[] = decode.split("/");
+  const bookId = parseInt(urlParams[1]);
+  const maxBooks = useSelector((state: BooksState) => get(state, ["books", "maxBooks"]));
   const dispatch = useDispatch();
-  // const book = useSelector((state: RootState) =>
-  //   get(state, ["bookLists", "bookTable", "278"])
-  // );
-  // const book = useSelector((state: RootState) => state.bookLists);
 
-  const testJson = { books: [
-    {
-    ISBN:"9784061529014",
-    author:"杉山将／著",
-    bookName:"機械学習のための確率と統計",
-    borrower: [],
-    exist:"一部発見",
-    find:3,
-    genre:"研究(理論)",
-    id:278,
-    location:"unidentified",
-    other:"なし",
-    pubdate:"20150408",
-    publisher:"講談社サイエンティフィク",
-    subGenre:"統計・機械学習",
-    sum:6,
-    withDisc:"なし",
-    },
-    {
-    ISBN:"9784061529021",
-    author:"岡谷貴之／著",
-    bookName:"深層学習",
-    borrower: [],
-    exist:"一部発見",
-    find:1,
-    genre:"研究(理論)",
-    id:279,
-    location:"unidentified",
-    other:"なし",
-    pubdate:"20150408",
-    publisher:"講談社サイエンティフィク",
-    subGenre:"ニューラルネットワーク",
-    sum:2,
-    withDisc:"なし",
-    },
-    ] }
-    const normalizeData = (data: BookLists): SavedBooks => {
-      const booksSchema = new schema.Entity("books", {}, { idAttribute: "id" });
-      const booksTable = get(normalize(data, [booksSchema]), [
-        "entities",
-        "books"
-      ]);
-      const booksIdList = get(normalize(data, [booksSchema]), ["result"]);
-      return { booksTable, booksIdList };
+  const normalizeData = (
+    data: BookLists
+  ): Pick<BooksState, "booksTable" | "booksIdList"> => {
+    const booksSchema = new schema.Entity("books", {}, { idAttribute: "id" });
+    const booksTable = get(normalize(data, [booksSchema]), [
+      "entities",
+      "books"
+    ]);
+    const booksIdList = get(normalize(data, [booksSchema]), ["result"]);
+    return { booksTable, booksIdList };
+  };
+
+  const getBookLists = async (page: number) => {
+    try {
+      dispatch(fetchBookLists.started({ pageIndex: page }));
+      // const response = await fetchSearch(payload);
+      const response = await fetch("http://localhost:3000/dummyData.json");
+      if (!response.ok) {
+        dispatch(
+          fetchBookLists.failed({
+            params: { pageIndex: page },
+            error: { statusCode: response.status }
+          })
+        );
+        return;
+      }
+      const json = await response.json();
+      // APIのreturnが books: {} なので.
+      const newData = normalizeData(json.books);
+      const result = { ...newData, maxBooks: json.max_books };
+      dispatch(fetchBookLists.done({ params: { pageIndex: page }, result }));
+    } catch (error) {
+      console.log(`Error fetcing in getBookLists: ${error}`);
+    }
+  };
+
+  const id = 280;
+  //1冊の情報だけ取得する
+  const getOneBook = async () => {
+    const payload = {
+      id: id,
     };
-    const newData = normalizeData(testJson.books);
-    const dis = async () => {
-      dispatch(fetchBookLists.started());
-      dispatch(fetchBookLists.done({ result: newData }));
-    };
-    dis();
-  // const bookTitle = "Book A"  //TODO:booklistからもらう必要
-  // const book = useSelector((state: RootState) => state.bookLists);
-  // const bookTitle = book[0].bookName;
-  const book = useSelector((state: RootState) =>
-    get(state, ["bookLists", "booksTable", "278", "bookName"])
+    try {
+      dispatch(fetchBookLists.started({ pageIndex: 0 }));
+      // const response = await fetchBookId(payload);
+      const response = await fetch("http://localhost:3000/dummyData.json");
+      if (!response.ok) {
+        dispatch(
+          fetchBookLists.failed({
+            params: { pageIndex: 0 },
+            error: { statusCode: response.status }
+          })
+        );
+        return;
+      }
+      const json = await response.json();
+
+      // APIのreturnが books: {} なので.
+      const newData = normalizeData(json.books);
+      console.log(newData);
+      const result = { ...newData, maxBooks: json.max_books };
+      dispatch(fetchBookLists.done({ params: { pageIndex: 0 }, result }));
+    } catch (error) {
+      console.log(`Error fetcing in getBookLists: ${error}`);
+    }
+  };
+
+
+  const bookTitle = useSelector((state: RootState) =>
+    get(state, ["books", "booksTable", bookId, "bookName"])
   );
-  // console.log(book);
-  // console.log(path);
-  // const bookTitle = book.bookName;
-  // const bookTitle = testJson.books[0].bookName
-  const bookTitle = book;
 
-  // const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-  //   // console.log("test");
-  //   console.log(inputValue);
-  //   console.log("window表示");
-    
-  //   // return (
-  //   // <ModalProvider>
-  //   //   <Dialog />
-  //   // </ModalProvider>
-  //   // );
-  // };
+  // console.log(bookTitle);
+  // if(bookTitle == undefined){
+  //   console.log("bookTitle == undefined");
+  // }
+
+  // const bookTitle = book;
+
+  useEffect(() => {
+    if(maxBooks == 0){
+      getBookLists(0);
+      console.log("maxBooks == 0");
+    // getOneBook();
+    }else{
+      console.log("booksTable is not undefined");
+    }
+  }, []);
 
   const handleOnChange = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -109,27 +124,29 @@ export const Borrow = ({ history }: AppProps) =>  {
     []
   );
 
-  return (
-    <>
-      <Header history={history} backLink="/" />
-      {/* <div>BorrowBookName</div> */}
-      <BookName>{bookTitle}</BookName>
-      {/* <div>Username form</div> */}
-      {/* <PopupWindow></PopupWindow> */}
-      {/* <SearchBox></SearchBox> */}
-      {/* <input  placeholder="UserName" /> */}
-      <BorrowUserNameInput placeValue="UserName" handleOnChange={handleOnChange}/>
-      {/* <button onClick={handleClick}>Borrow</button> */}
-      <ModalProvider>
-        <BorrowButton history={history} buttonName="Borrow" bookTitle={bookTitle} borrower={borrower}/>
-      </ModalProvider>
-      {/* <SearchIcon handleClick={handleClick} /> */}
-      {/* <div>Borrow Button</div> */}
-      {/* <ModalProvider>
-        <Dialog />
-      </ModalProvider> */}
-    </>
-  );
+  //Debug
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    getOneBook();
+  }
+  
+  if(bookTitle != undefined){
+    return (
+      <>
+        <Header history={history} backLink="/" />
+        <BookName>{bookTitle}</BookName>
+        <BorrowUserNameInput placeValue="UserName" handleOnChange={handleOnChange}/>
+        <ModalProvider>
+          <BorrowButton history={history} buttonName="Borrow" bookTitle={bookTitle} borrower={borrower} bookId={bookId}/>
+        </ModalProvider>
+
+        <button onClick={handleClick}>get 280</button>
+      </>
+      );
+    }else{
+      return (
+        <div>error</div>
+      );
+  }
 };
 
 export default Borrow;
